13b899e58ac6144ff55fc6571c5b88868dbf45d4
Timir Gazizov <abzxcvvcxzba@yandex.ru>
2024-02-15 01:24:04 +0300

    Correct code due to browser fixture delete

--- tests/test_check_cards_search.py
+++ tests/test_check_cards_search.py
@@ -2,10 +2,11 @@ from allure_commons.types import AttachmentType
 from random import randint
 from pokemon_API_project_tests.utils.api_methods import api_method
 from pokemon_API_project_tests.utils.browser_actions import browser_action
+from selenium import webdriver
 import allure
 
 
-def test_check_cards_search_by_exact_matching_name(browser, base_url, search):
+def test_check_cards_search_by_exact_matching_name(base_url, search):
     with allure.step("Get cards list by search request"):
         response = api_method.search_cards_by_exact_matching_name(base_url, search)
 
@@ -21,12 +22,18 @@ def test_check_cards_search_by_exact_matching_name(browser, base_url, search):
         cards_amount = len(response['data'])
         n = randint(0, cards_amount - 1)
         image_link = response["data"][n]["images"]["large"]
+        browser = webdriver.Chrome()
         browser.get(image_link)
 
+        def quit_browser():
+            browser.close()
+            browser.quit()
+
         if browser_action.element_is_present(browser, '//img'):
-            allure.attach(image_link, name="Image link", attachment_type=AttachmentType.TEXT)
+            allure.attach(image_link, name=f'Random card found by search request "{search}"', attachment_type=AttachmentType.TEXT)
             allure.attach(browser.get_screenshot_as_png(), name="Screenshot", attachment_type=AttachmentType.PNG)
-            pass
+            quit_browser()
         else:
             allure.attach(browser.get_screenshot_as_png(), name="Screenshot", attachment_type=AttachmentType.PNG)
+            quit_browser()
             assert False, "Error while loading card."
