d7ed851ac3af0aec968b3c3932cad951d47bcc0b
Timir Gazizov <abzxcvvcxzba@yandex.ru>
2024-02-17 16:05:20 +0300

    Refactor code

--- tests/test_check_cards_search_by_exact_matching_name.py
+++ tests/test_check_cards_search_by_exact_matching_name.py
@@ -1,7 +1,7 @@
 from allure_commons.types import AttachmentType
-from random import randint
 from pokemon_API_project_tests.utils.api_methods import api_method
 from pokemon_API_project_tests.utils.browser_actions import browser_action
+from pokemon_API_project_tests.utils.response_actions import response_action
 import allure
 
 
@@ -12,26 +12,20 @@ def test_check_cards_search_by_exact_matching_name(base_url, search):
     with allure.step("Check response status code"):
         assert response.status_code == 200, f"Response status code is {response.status_code} instead of 200."
 
-    with allure.step("Check that response Pokemon names match search request"):
+    with allure.step("Check that response' Pokemon names exact match search request"):
         response = response.json()
-        n = len(response['data'])
-        for i in range(n):
-            pokemon_name = response['data'][i]['name']
-            assert search.lower() == pokemon_name.lower(), (f'Search request: "{search}", '
-                                                            f'one of search results: "{pokemon_name}".')
+        response_action.check_search_results_exact_match_search_request(response, search)
 
-    with allure.step("Open random card from search results in browser if possible"):
-        cards_amount = len(response['data'])
-        n = randint(0, cards_amount - 1)
-        image_link = response["data"][n]["images"]["large"]
+    with allure.step("Open random card from search results in browser"):
+        image_link = response_action.get_random_card_link(response)
 
         browser = browser_action.setup_browser()
         browser_action.open_link(browser, image_link)
 
         if browser_action.element_is_present(browser, '//img'):
-            allure.attach(image_link, name=f'Random card found by search request "{search}"',
-                          attachment_type=AttachmentType.TEXT)
-            allure.attach(browser.get_screenshot_as_png(), name="Screenshot", attachment_type=AttachmentType.PNG)
+            allure.attach(browser.get_screenshot_as_png(), name=f'One of the cards found by search request "{search}"',
+                          attachment_type=AttachmentType.PNG)
+            allure.attach(image_link, name='Card link', attachment_type=AttachmentType.TEXT)
             browser_action.quit_browser(browser)
         else:
             allure.attach(browser.get_screenshot_as_png(), name="Screenshot", attachment_type=AttachmentType.PNG)
