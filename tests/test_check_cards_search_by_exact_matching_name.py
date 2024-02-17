0b713488ffa30c8c2857b3b36ec3bbfab89adcbc
Timir Gazizov <abzxcvvcxzba@yandex.ru>
2024-02-17 15:46:49 +0300

    Add Pokemon names checking

--- tests/test_check_cards_search_by_exact_matching_name.py
+++ tests/test_check_cards_search_by_exact_matching_name.py
@@ -11,29 +11,30 @@ def test_check_cards_search_by_exact_matching_name(base_url, search):
         response = api_method.search_cards_by_exact_matching_name(base_url, search)
 
     with allure.step("Check response status code"):
-        assert response.status_code == 200
+        assert response.status_code == 200, f"Response status code is {response.status_code} instead of 200."
 
-    with allure.step("Check first result's Pokemon name"):
+    with allure.step("Check that response Pokemon names match search request"):
         response = response.json()
-        pokemon_name = response['data'][0]['name']
-        assert search.lower() == pokemon_name.lower()
+        n = len(response['data'])
+        for i in range(n):
+            pokemon_name = response['data'][i]['name']
+            assert search.lower() == pokemon_name.lower(), (f'Search request: "{search}", '
+                                                            f'one of search results: "{pokemon_name}".')
 
     with allure.step("Open random card from search results in browser if possible"):
         cards_amount = len(response['data'])
         n = randint(0, cards_amount - 1)
         image_link = response["data"][n]["images"]["large"]
-        browser = webdriver.Chrome()
-        browser.get(image_link)
 
-        def quit_browser():
-            browser.close()
-            browser.quit()
+        browser = browser_action.setup_browser()
+        browser_action.open_link(browser, image_link)
 
         if browser_action.element_is_present(browser, '//img'):
-            allure.attach(image_link, name=f'Random card found by search request "{search}"', attachment_type=AttachmentType.TEXT)
+            allure.attach(image_link, name=f'Random card found by search request "{search}"',
+                          attachment_type=AttachmentType.TEXT)
             allure.attach(browser.get_screenshot_as_png(), name="Screenshot", attachment_type=AttachmentType.PNG)
-            quit_browser()
+            browser_action.quit_browser(browser)
         else:
             allure.attach(browser.get_screenshot_as_png(), name="Screenshot", attachment_type=AttachmentType.PNG)
-            quit_browser()
+            browser_action.quit_browser(browser)
             assert False, "Error while loading card."
