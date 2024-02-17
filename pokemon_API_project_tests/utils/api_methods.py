ca7ee08c601fd738d309b7ac4f411e05b31f6d9a
Timir Gazizov <abzxcvvcxzba@yandex.ru>
2024-02-17 15:27:41 +0300

    Refactor code

--- pokemon_API_project_tests/utils/api_methods.py
+++ pokemon_API_project_tests/utils/api_methods.py
@@ -6,8 +6,7 @@ from allure_commons.types import AttachmentType
 
 
 class APIMethods:
-    def get_cards_list(self, base_url):
-        response = requests.get(url=base_url)
+    def attach_logs_and_response_info(self, response):
         allure.attach(body=response.request.url, name="Request URL", attachment_type=AttachmentType.TEXT)
         allure.attach(body=response.request.method, name="Request method", attachment_type=AttachmentType.TEXT)
         allure.attach(body=str(response.status_code), name="Response status code", attachment_type=AttachmentType.TEXT,
@@ -17,32 +16,20 @@ class APIMethods:
         logging.info("Request: " + response.request.url)
         logging.info("Response code " + str(response.status_code))
         logging.info("Response: " + response.text)
+
+    def get_cards_list(self, base_url):
+        response = requests.get(url=base_url)
+        self.attach_logs_and_response_info(response)
         return response
 
     def search_cards_by_exact_matching_name(self, base_url, search_request):
         response = requests.get(url=base_url + f'?q=!name:{search_request}')
-        allure.attach(body=response.request.url, name="Request URL", attachment_type=AttachmentType.TEXT)
-        allure.attach(body=response.request.method, name="Request method", attachment_type=AttachmentType.TEXT)
-        allure.attach(body=str(response.status_code), name="Response status code", attachment_type=AttachmentType.TEXT,
-                      extension='txt')
-        allure.attach(body=json.dumps(response.json(), indent=4, ensure_ascii=True), name="Response body",
-                      attachment_type=AttachmentType.JSON, extension="json")
-        logging.info("Request: " + response.request.url)
-        logging.info("Response code " + str(response.status_code))
-        logging.info("Response: " + response.text)
+        self.attach_logs_and_response_info(response)
         return response
 
     def get_card_by_id(self, base_url, id):
         response = requests.get(url=base_url + f'/{id}')
-        allure.attach(body=response.request.url, name="Request URL", attachment_type=AttachmentType.TEXT)
-        allure.attach(body=response.request.method, name="Request method", attachment_type=AttachmentType.TEXT)
-        allure.attach(body=str(response.status_code), name="Response status code", attachment_type=AttachmentType.TEXT,
-                      extension='txt')
-        allure.attach(body=json.dumps(response.json(), indent=4, ensure_ascii=True), name="Response body",
-                      attachment_type=AttachmentType.JSON, extension="json")
-        logging.info("Request: " + response.request.url)
-        logging.info("Response code " + str(response.status_code))
-        logging.info("Response: " + response.text)
+        self.attach_logs_and_response_info(response)
         return response
 
 
