ede6e2937732bc3f6cdbdb04e3765ee7aa6eeeb8
Timir Gazizov <abzxcvvcxzba@yandex.ru>
2024-02-18 17:55:48 +0300

    Add condition for 1 page

--- pokemon_API_project_tests/utils/api_methods.py
+++ pokemon_API_project_tests/utils/api_methods.py
@@ -18,17 +18,17 @@ class APIMethods:
         logging.info("Response code: " + str(response.status_code))
         logging.info("Response: " + response.text)
 
-    def get_cards_list(self, base_url):
+    def get_cards_or_sets_list(self, base_url):
         response = requests.get(url=base_url)
         self.attach_logs_and_response_info(response)
         return response
 
-    def search_cards_by_exact_matching_name(self, base_url, search_request):
+    def search_cards_or_sets_by_exact_matching_name(self, base_url, search_request):
         response = requests.get(url=base_url + f'?q=!name:{search_request}')
         self.attach_logs_and_response_info(response)
         return response
 
-    def get_card_by_id(self, base_url, id):
+    def get_card_or_set_by_id(self, base_url, id):
         response = requests.get(url=base_url + f'/{id}')
         self.attach_logs_and_response_info(response)
         return response
@@ -42,8 +42,13 @@ class APIMethods:
         last_page_count = total_cards % on_page
         if last_page_count > 0:
             pages += 1
-        n = randint(0, pages - 1)
-        response = self.get_cards_list(base_url + f'?page={n}')
+
+        if pages == 1:
+            n = 1
+        else:
+            n = randint(0, pages - 1)
+
+        response = self.get_cards_or_sets_list(base_url + f'?page={n}')
         opened_page = response.json()["page"]
         assert opened_page == n, f'Opened page is {opened_page} instead of {n}.'
         return response
